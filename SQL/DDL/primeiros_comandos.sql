a0f63339fc9ce74d31d5eaf824798a93f27c660c
Samuel Henricky <samuel.gonzaga.dev@gmail.com>
2026-04-26 21:20:17 -0300

    first commit

---
+++ SQL/DDL/primeiros_comandos.sql
@@ -0,0 +1,14 @@
+# Mostra todos os databases existentes
+show databases;
+
+# Cria um novo database
+create database db_sistema_pedidos;
+
+# Apaga um database existente
+drop database db_sistema_pedidos;
+
+# Ativa o database a ser utilizado
+use db_sistema_pedidos;
+
+# Permite visualizar todas as tabelas existentes no database
+show tables;
