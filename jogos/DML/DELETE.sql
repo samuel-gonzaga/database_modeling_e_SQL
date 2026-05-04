642cbe8249151f9a675b9aa956feb8991a29bccd
Samuel Henricky <samuel.gonzaga.dev@gmail.com>
2026-05-04 19:17:50 -0300

    update and delete commands

---
+++ jogos/DML/DELETE.sql
@@ -0,0 +1,6 @@
+USE ead_unifecaf;
+
+/*EXCLUSÃO DE DADOS EM UMA TABELA*/
+DELETE FROM tbl_jogos WHERE id_jogo = 10;
+
+SELECT * FROM tbl_jogos;
---
+++ jogos/DML/UPDATE.sql
@@ -0,0 +1,6 @@
+USE ead_unifecaf;
+
+/*ATUALIZAÇÃO DE UM REGISTRO NA TABELA CRIADA*/
+UPDATE tbl_jogos SET nome_jogo = 'Super Mário Bross' WHERE id_jogo = 3;
+
+SELECT * FROM tbl_jogos;
