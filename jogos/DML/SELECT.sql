1636b562b02c751e817fee3210d464c22bb24cf8
Samuel Henricky <samuel.gonzaga.dev@gmail.com>
2026-05-05 16:36:08 -0300

    more selects

--- jogos/DML/SELECT.sql
+++ jogos/DML/SELECT.sql
@@ -11,3 +11,8 @@ select j.nome_jogo, j.genero, p.nome_plataforma from tbl_jogos j
 	inner join tbl_plataformas p
     on j.fk_plataforma = p.id_plataforma
     where j.genero = 'RPG';
+    
+select j.nome_jogo, j.valor_jogo, p.nome_plataforma from tbl_jogos j
+	inner join tbl_plataformas p
+    on j.fk_plataforma = p.id_plataforma
+    where j.valor_jogo > 150;
