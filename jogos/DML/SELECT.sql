be3fb06dbbfb1dc6906d77a51e8b1de329030075
Samuel Henricky <samuel.gonzaga.dev@gmail.com>
2026-05-05 16:33:45 -0300

    add select searchs

---
+++ jogos/DML/SELECT.sql
@@ -0,0 +1,13 @@
+use ead_unifecaf;
+
+select * from tbl_jogos;
+select * from tbl_plataformas;
+
+select nome_jogo, nome_plataforma from tbl_jogos
+	inner join tbl_plataformas
+    ON tbl_jogos.fk_plataforma = tbl_plataformas.id_plataforma;
+    
+select j.nome_jogo, j.genero, p.nome_plataforma from tbl_jogos j
+	inner join tbl_plataformas p
+    on j.fk_plataforma = p.id_plataforma
+    where j.genero = 'RPG';
