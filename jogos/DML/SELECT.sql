f708db890547c855a8d65e96ba4d453d3b13fb00
Samuel Henricky <samuel.gonzaga.dev@gmail.com>
2026-05-05 16:44:36 -0300

    some selects with joins

--- jogos/DML/SELECT.sql
+++ jogos/DML/SELECT.sql
@@ -16,3 +16,25 @@ select j.nome_jogo, j.valor_jogo, p.nome_plataforma from tbl_jogos j
 	inner join tbl_plataformas p
     on j.fk_plataforma = p.id_plataforma
     where j.valor_jogo > 150;
+
+# Left join
+SELECT j.nome_jogo, p.nome_plataforma
+FROM tbl_jogos j
+LEFT JOIN tbl_plataformas p ON j.fk_plataforma = p.id_plataforma;
+
+# Right join
+SELECT j.nome_jogo, p.nome_plataforma
+FROM tbl_jogos j
+RIGHT JOIN tbl_plataformas p ON j.fk_plataforma = p.id_plataforma;
+
+# Full Join
+SELECT j.nome_jogo, p.nome_plataforma
+FROM tbl_jogos j
+LEFT JOIN tbl_plataformas p ON j.fk_plataforma = p.id_plataforma
+
+UNION
+
+SELECT j.nome_jogo, p.nome_plataforma
+FROM tbl_jogos j
+RIGHT JOIN tbl_plataformas p ON j.fk_plataforma = p.id_plataforma;
+
